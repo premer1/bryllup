eda798448b4c60ce6df5396576ad921f78097e67
Alexander Premer <alexander.premer@entur.org>
2024-03-17 16:33:10 +0100

    legg-til-dresskode: La til dresskode

--- src/routes/PraktiskInfo.jsx
+++ src/routes/PraktiskInfo.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import Tryvannstua from '../images/tryvannstua.png'
+
 const PraktiskInfo = () => {
-  return (
-    <div className='flex flex-col items-center justify-center mt-10'>
-      <div>
-        <h1 className='text-6xl'>Praktisk Info</h1>
-      </div>
+    return (
+        <div className='flex flex-col items-center justify-center mt-10'>
+            <div>
+                <h1 className='text-6xl'>Praktisk Info</h1>
+            </div>
 
-      <div className='flex flex-col justify-center items-center mt-4 p-3 text-xl'>
-      <h2>Bryllupet feires på Tryvannstua 29. juni</h2>
-      <img src={Tryvannstua} alt="img" className='h-80 rounded mt-2'/>
-      </div>
+            <div className='flex flex-col justify-center items-center mt-4 p-3 text-xl'>
+                <h2>Bryllupet feires på Tryvannstua 29. juni</h2>
+                <img src={Tryvannstua} alt="img" className='h-80 rounded mt-2'/>
+            </div>
 
-      <div className='flex flex-col justify-center items-center mt-4 text-center text-xl'>
-        <h2>Felles busser går fra Oslo S, Sjøsiden, kl.14</h2>
-        <h2 className='mt-1'>Det er satt opp to busser tilbake til Oslo S, kl. 23 og kl. 02.00</h2>
-      </div>
+            <div className='flex flex-col justify-center items-center mt-4 text-center text-xl'>
+                <h2>Felles busser går fra Oslo S, Sjøsiden, kl.14</h2>
+                <h2 className='mt-1'>Det er satt opp to busser tilbake til Oslo S, kl. 23 og kl. 02.00</h2>
+            </div>
 
-      <h2 className='mt-10 text-center text-l'>Lurt å ta med jakke <br />For Tøffinger - Ta med badetøy og håndkle</h2>
+            <h2 className='mt-10 text-center text-l'>Dresskoden er dress</h2>
+            <h2 className='mt-0 text-center text-l'>Lurt å ta med jakke <br/>For Tøffinger - Ta med badetøy og håndkle
+            </h2>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default PraktiskInfo
